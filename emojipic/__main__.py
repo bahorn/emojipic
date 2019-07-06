baf61cffa969155a476fe8aa939b3d5ff4063e7b
bahorn <hornb@uni.coventry.ac.uk>
2019-07-06 04:27:21 +0100

    Added webcam support

--- emojipic/__main__.py
+++ emojipic/__main__.py
@@ -13,17 +13,22 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Displays Images or Videos as Emojis in the terminal')
     parser.add_argument('filename', type=str, help='Filename')
+    parser.add_argument('--webcam', action='store_true',
+                        help='Use the webcam specified')
     parser.add_argument('--resize', type=int, default=32,
                         help='How many characters to scale this to')
     args = parser.parse_args()
 
-    # determine if we are going to treat it like a video or a image
-    filetype = magic.from_file(args.filename, mime=True)
-    if filetype in formats.pillow_supported_formats:
-        ei = pic.EmojiImage()
-        ei.open(args.filename)
-        print(ei.make(args.resize)[0])
-    elif filetype in formats.opencv_supported_formats:
-        ani.main(args.filename, args.resize)
+    if args.webcam:
+        ani.main(int(args.filename), args.resize)
     else:
-        print('Format not supported')
+        # determine if we are going to treat it like a video or a image
+        filetype = magic.from_file(args.filename, mime=True)
+        if filetype in formats.pillow_supported_formats:
+            ei = pic.EmojiImage()
+            ei.open(args.filename)
+            print(ei.make(args.resize)[0])
+        elif filetype in formats.opencv_supported_formats:
+            ani.main(args.filename, args.resize)
+        else:
+            print('Format not supported')
